78260749ca4688b2ee44fd92ab83a05cec309132
Steven Araujo <saraujo@espol.edu.ec>
2017-10-28 18:25:47 -0500

    cambio colores

--- src/sorteofisica/SorteoFisica.java
+++ src/sorteofisica/SorteoFisica.java
@@ -17,6 +17,7 @@ public class SorteoFisica {
         obj.Ejercicios(cantidad, lis);
         
         System.out.println("*******************");
+        System.out.println("*******************");
         
         
     }
